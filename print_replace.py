3528130c9da4f6aad799ce6c2e0a23e4d690b868
Jordi París-López <jordiparis92@gmail.com>
2019-07-23 15:06:07 +0200

    Add files via upload

--- print_replace.py
+++ print_replace.py
@@ -6,16 +6,33 @@
 
     The code will print "Loading" and introducing dots every second.
     Right after all the desired prints are in the output, the print shall
-    erased.
+    erased. This procedure is performed twice.
 
 """
 
-
-# Import time library
+# Import time and sys libraries
 import time
+import sys
 
-# Initialise printing loop
-for x in range (0,5):             # Maximum of 5 dots in the print.
-    p = "Loading" + "." * x       # Assign Loading and additional points to p.
-    print (p, end = "\r")         # Prints p erasing the previous line.
-    time.sleep(1)                 # Pauses the code 1 second for easier visualisation.  
+# Define the commands to control terminal outputs.
+CURSOR_UP_ONE = '\x1b[1A'
+ERASE_LINE = '\x1b[2K'
+sys.stdout.write(CURSOR_UP_ONE)
+sys.stdout.write(ERASE_LINE)
+
+# Define function that deletes the last lines (n)
+def delete_last_lines(n = 1):
+    for _ in range(n):
+        sys.stdout.write(CURSOR_UP_ONE)
+        sys.stdout.write(ERASE_LINE)
+
+
+# Initialise iteration loop (2 iterations)
+for i in range(0,2):
+    # Initialise printing loop
+    for x in range (0,5):             # Maximum of 5 dots in the print.
+        p = "Loading" + "." * x       # Assign Loading and additional points to p.
+        print (p, end = "\r")         # Prints p erasing the previous line.
+        time.sleep(1)                 # Pauses the code 1 second for easier visualisation.
+    print("   ")                      # String of space to immediately delete
+    delete_last_lines()
